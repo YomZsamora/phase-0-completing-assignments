4ede5049c09f15a85bf2787f4ef9f613464a53eb
yomZsamora <samaurah@gmail.com>
2022-07-29 12:04:16 +0300

    Installed Packages. All tests are running.

--- test/index-test.js
+++ test/index-test.js
@@ -7,8 +7,7 @@ describe( 'This assignment', () => {
   it( 'has been correctly cloned to your local environment', () => {
     chai.assert.isDirectory( './.git', 'no ".git" folder was found within "welcome-completing-assignment". Use "git init" to create one' );
   } )
-} )
-
+})
 
 
 
